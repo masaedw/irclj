127ca8836b7238fb8e60fcbb3d5b727b515d0c36
武藤 雅幸 <masa.edw@gmail.com>
2010-03-22 00:28:01 +0900

    add icu4j

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject irclj "0.1.0-SNAPSHOT"
   :description "IRC Library for Clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [com.ibm.icu/icu4j "4.0.1"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
