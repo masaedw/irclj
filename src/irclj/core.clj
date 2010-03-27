19a66d8282496fc80ad01796b7248bbcf53c2380
武藤 雅幸 <masa.edw@gmail.com>
2010-03-28 02:12:04 +0900

    fixed patterns and added conversion functions

--- src/irclj/core.clj
+++ src/irclj/core.clj
@@ -23,7 +23,7 @@
 (def LETTER "A-Za-z")
 (def DIGIT "\\d")
 (def HEXDIGIT (str DIGIT "A-Fa-f"))
-(def SPECIAL "\u005B-\u0060\u007B-\u007D")
+(def SPECIAL "\\x5B-\\x60\\x7B-\\x7D")
 
 ;; shortname ( letter / digit ) *( letter / digit / "-" )
 ;;               *( letter / digit )
@@ -36,50 +36,72 @@
 (def SERVERNAME HOSTNAME)
 
 ;; nickname   =  ( letter / special ) *8( letter / digit / special / "-" )
-(def NICKNAME (str "[" LETTER SPECIAL"][-" LETTER DIGIT SPECIAL "]*"))
+(def NICKNAME (str "[" LETTER SPECIAL "][-" LETTER DIGIT SPECIAL "]*"))
 
 ;; user       =  1*( %x01-09 / %x0B-0C / %x0E-1F / %x21-3F / %x41-FF )
 ;;                 ; any octet except NUL, CR, LF, " " and "@"
-(def USER "[\u0001-\u0009\u000B-\u000C\u000E-\u001F\u0021-\u003F\u0041-\u00FF]+")
+(def USER "[\\x01-\\x09\\x0B-\\x0C\\x0E-\\x1F\\x21-\\x3F\\x41-\\xFF]+")
 
 ;; ip4addr    =  1*3digit "." 1*3digit "." 1*3digit "." 1*3digit
-(def IP4ADDR "[#{DIGIT}]{1,3}(?:\\.[#{DIGIT}]{1,3}){3}")
+(def IP4ADDR (str "[" DIGIT" ]{1,3}(?:\\.[" DIGIT "]{1,3}){3}"))
 ;; ip6addr    =  1*hexdigit 7( ":" 1*hexdigit )
 ;; ip6addr    =/ "0:0:0:0:0:" ( "0" / "FFFF" ) ":" ip4addr
-(def IP6ADDR "(?:[#{HEXDIGIT}]+(?::[#{HEXDIGIT}]+){7}|0:0:0:0:0:(?:0|FFFF):#{IP4ADDR})")
+(def IP6ADDR (str "(?:[" HEXDIGIT "]+(?::[" HEXDIGIT "]+){7}|0:0:0:0:0:(?:0|FFFF):" IP4ADDR ")"))
 ;; hostaddr   =  ip4addr / ip6addr
-(def HOSTADDR "(?:#{IP4ADDR}|#{IP6ADDR})")
+(def HOSTADDR (str "(?:" IP4ADDR "|" IP6ADDR ")"))
 
 ;; host       =  hostname / hostaddr
-(def HOST "(?:#{HOSTNAME}|#{HOSTADDR})")
+(def HOST (str "(?:" HOSTNAME "|" HOSTADDR ")"))
 
 ;; prefix     =  servername / ( nickname [ [ "!" user ] "@" host ] )
-(def PREFIX "(?:#{NICKNAME}(?:(?:!#{USER})?@#{HOST})?|#{SERVERNAME})")
+(def PREFIX (str "(?:" NICKNAME "(?:(?:!" USER ")?@" HOST ")?|" SERVERNAME ")"))
 
 ;; nospcrlfcl =  %x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
 ;;                 ; any octet except NUL, CR, LF, " " and ":"
-(def NOSPCRLFCL "\u0001-\u0009\u000B-\u000C\u000E-\u001F\u0021-\u0039\u003B-\u00FF")
+(def NOSPCRLFCL "\\x01-\\x09\\x0B-\\x0C\\x0E-\\x1F\\x21-\\x39\\x3B-\\xFF")
 
 ;; command    =  1*letter / 3digit
-(def COMMAND "(?:[#{LETTER}]+|[#{DIGIT}]{3})")
+(def COMMAND (str "(?:[" LETTER "]+|[" DIGIT "]{3})"))
 
 ;; SPACE      =  %x20        ; space character
 ;; middle     =  nospcrlfcl *( ":" / nospcrlfcl )
 ;; trailing   =  *( ":" / " " / nospcrlfcl )
 ;; params     =  *14( SPACE middle ) [ SPACE ":" trailing ]
 ;;            =/ 14( SPACE middle ) [ SPACE [ ":" ] trailing ]
-(def MIDDLE "[#{NOSPCRLFCL}][:#{NOSPCRLFCL}]*")
-(def TRAILING "[: #{NOSPCRLFCL}]*")
-(def PARAMS "(?:((?: l+#{MIDDLE}){0,14})(?: +:(#{TRAILING}))?|((?: +#{MIDDLE}){14}):?(#{TRAILING}))")
+(def MIDDLE (str "[" NOSPCRLFCL "][:" NOSPCRLFCL "]*"))
+(def TRAILING (str "[: " NOSPCRLFCL "]*"))
+(def PARAMS (str "(?:((?: +" MIDDLE "){0,14})(?: +:(" TRAILING "))?|((?: +" MIDDLE "){14}):?(" TRAILING "))"))
 
 ;; crlf       =  %x0D %x0A   ; "carriage return" "linefeed"
 ;; message    =  [ ":" prefix SPACE ] command [ params ] crlf
-(def CRLF "\u000D\u000A")
-(def MESSAGE "(?::(#{PREFIX}) +)?(#{COMMAND})#{PARAMS}\\s*(#{CRLF}|\n|\r)")
+(def CRLF "\\x0D\\x0A")
+(def MESSAGE (str "(?::(" PREFIX ") +)?(" COMMAND ")" PARAMS "\\s*(" CRLF "|\\n|\\r)"))
 
-(def CLIENT_PATTERN  #"^NICKNAME(?:(?:!USER)?@HOST)$")
-(def MESSAGE_PATTERN #"^MESSAGE$")
+(def CLIENT_PATTERN  (re-pattern (str "^" NICKNAME "(?:(?:!" USER ")?@ "HOST ")$")))
+(def MESSAGE_PATTERN (re-pattern (str "^" MESSAGE "$")))
 
+(defn build-message
+  [prefix command params]
+  {:prefix prefix
+   :command command
+   :params params})
+
+(defn line->message
+  [line]
+  (let [[src _1 _2 _3 _4 _5 _6] (re-find MESSAGE_PATTERN line)]
+    (cond (nil? src)
+            nil
+          (not (empty? _3))
+            (build-message _1 _2 (concat (rest (.split _3 " ")) (list _4)))
+          (not (empty? _5))
+            (build-message _1 _2 (concat (rest (.split _5 " ")) (list _6)))
+          (not (empty? _4))
+            (build-message _1 _2 (list _4))
+          (not (empty? _6))
+            (build-message _1 _2 (list _6))
+          :else
+            (build-message _1 _2 '())
+          )))
 
 (defn raw-command-seq
   [istream]
