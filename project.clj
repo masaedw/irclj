eb0e7f88105992df5198afb3af0b2cc6d09ae45f
masaedw <masa.edw@gmail.com>
2010-05-08 10:19:12 +0900

    clojure1.1 -> 1.2.0-snapshot

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject irclj "0.1.0-SNAPSHOT"
   :description "IRC Library for Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [com.ibm.icu/icu4j "4.0.1"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
   :main irclj)
