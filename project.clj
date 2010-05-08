9c5683120ecdb0f3dc5ddd59a16d1ce86287d389
masaedw <masa.edw@gmail.com>
2010-05-08 10:41:38 +0900

    lein-swank 1.1.0 -> 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [com.ibm.icu/icu4j "4.0.1"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
   :main irclj)
