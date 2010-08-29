b128679f3bee478930521ed7b821550e38107298
masaedw <masa.edw@gmail.com>
2010-08-29 19:40:17 +0900

    re-implement sock-read

--- src/irclj/io.clj
+++ src/irclj/io.clj
@@ -22,9 +22,18 @@
          (.close s#)
          )))))
 
-(defn sock-read
-  "ソケットから読み出せるデータを全部読む (TODO: 正しく実装する)"
+(defn- read-available
   [^java.io.InputStream is]
-  (let [buf (make-array Byte/TYPE 2000) ;; 全部読んでない!
+  (let [buf (make-array Byte/TYPE 2000)
         len (.read is buf)]
-    (take len buf)))
+    (if (not (= len -1))
+      (take len buf)
+      (.close is))))
+
+(defn sock-read
+  "ソケットから読み出せるデータを全部読んで、バイトシーケンスとして返す"
+  [^java.io.InputStream is]
+  (letfn [(step [is]
+                (when-let [value (read-available is)]
+                  (concat value (sock-read is))))]
+    (lazy-seq (step is))))
